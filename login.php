e3514f81e375c0d7fca049f6c48a878f4e0428d8
KarolMidd <124504262+KarolMidd@users.noreply.github.com>
2024-06-13 09:45:04 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,31 +1,36 @@
 <?php
-
+session_start();
 include 'pma.php';
-$login = $_POST['Login'];
-$haslo = $_POST['Pass'];
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $login = $_POST['Login'];
+    $haslo = $_POST['Pass'];
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-
-
-$sql = "SELECT Nazwa, Haslo, Rola, Id FROM pracownicy WHERE Nazwa = '$login'";
-
-    $result = $conn->query($sql);
-    $row = $result->fetch_assoc();
-    $verify = password_verify($haslo, $row['Haslo']);
-
-    if ($verify) {
-        $row = $result->fetch_assoc();
-        $_SESSION['role'] = $row['Rola'];
-        $_SESSION['id'] = $row['Id'];
-        header('Location: Grafik.php');
-        exit();
-    } else {
-        echo "<script> alert('Błędne dane logowania')</script>";
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
+
+    $sql = "SELECT Nazwa, Haslo, Rola, Id FROM pracownicy WHERE Nazwa = '$login'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $verify = password_verify($haslo, $row['Haslo']);
+
+        if ($verify) {
+            $_SESSION['role'] = $row['Rola'];
+            $_SESSION['id'] = $row['Id'];
+            header('Location: Grafik.php');
+            exit();
+        } else {
+            echo "<script> alert('Błędne dane logowania'); window.location.href='Start.php'; </script>";
+        }
+    } else {
+        echo "<script> alert('Błędne dane logowania'); window.location.href='Start.php'; </script>";
+    }
+
     $conn->close();
+}
 ?>
