85f0c3fa4d11ee2702bdd90b893c17c0de9caf9a
Kacperf19 <95441320+Kacperf19@users.noreply.github.com>
2024-06-18 09:39:38 +0200

    Update editOrder.php

--- editOrder.php
+++ editOrder.php
@@ -21,7 +21,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Order</title>
     <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="Order.css">
+    <link rel="stylesheet" href="EditOrder.css">
 </head>
 <body>
     <section>
